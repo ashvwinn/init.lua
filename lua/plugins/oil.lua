bd0b4c742c0b8b9a2dcc2274b1a02867bc9140a8
Ashvin Jangid <142579833+ASH-WIN-10@users.noreply.github.com>
2026-03-29 19:39:52 +0530

    remove oil as the default file_explorer

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,9 +1,9 @@
 return {
-    'stevearc/oil.nvim',
+    "stevearc/oil.nvim",
     ---@module 'oil'
     ---@type oil.SetupOpts
     opts = {
-        default_file_explorer = true,
+        default_file_explorer = false,
         view_options = {
             show_hidden = true,
         },
